e38e898eccd190218e901c9a84bc1c6336304994
James Held <james.held2@gmail.com>
2022-07-04 14:55:04 -0400

    clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -36,13 +36,17 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    //clear users from state
+    const clearUsers = () => dispatch({type: "CLEAR_USERS"})
+
     //set loading
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
             users: state.users,
             loading: state.loading,
-            searchUsers
+            searchUsers,
+            clearUsers
         }}>
         {children}
     </GithubContext.Provider>
