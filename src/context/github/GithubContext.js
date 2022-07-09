986cf59bd3e441367eeee99b4fd6bfdad01dddf2
James Held <james.held2@gmail.com>
2022-07-09 18:11:29 -0400

    move searchUsers to GithubActions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,28 +16,6 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    //get users
-    const searchUsers = async (text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-
-        const {items} = await response.json()
-
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        })
-    }
-
         //get single results
         const getUser = async (login) => {
             setLoading()
@@ -92,11 +70,8 @@ export const GithubProvider = ({children}) => {
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
-            users: state.users,
-            loading: state.loading,
-            user: state.user,
-            repos: state.repos,
-            searchUsers,
+            ...state,
+            dispatch,
             clearUsers,
             getUser,
             getUserRepos,
